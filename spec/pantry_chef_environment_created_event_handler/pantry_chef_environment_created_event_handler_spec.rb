a3efb8f61f2db265b9c60ae933c9bed2fbc9de08
Kieran Manning <kieran.manning@wonga.com>
2013-09-11 18:58:35 +0100

    initial commit

---
+++ spec/pantry_chef_environment_created_event_handler/pantry_chef_environment_created_event_handler_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require_relative '../../pantry_chef_environment_created_event_handler/pantry_chef_environment_created_event_handler'
+
+describe Wonga::Daemon::PantryChefEnvironmentCreatedEventHandler do
+  it_behaves_like 'handler'
+  describe "#handle_message" do
+    #add in unit test code for handle message here.
+  end
+end
+
