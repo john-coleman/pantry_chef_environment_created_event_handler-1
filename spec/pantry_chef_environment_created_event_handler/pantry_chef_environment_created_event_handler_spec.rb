fe65cc5612a11b4feee51e24be0a399ef6c8d8b8
Kieran Manning <kieran.manning@wonga.com>
2013-09-17 12:00:24 +0100

    functionality added

--- spec/pantry_chef_environment_created_event_handler/pantry_chef_environment_created_event_handler_spec.rb
+++ spec/pantry_chef_environment_created_event_handler/pantry_chef_environment_created_event_handler_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 require_relative '../../pantry_chef_environment_created_event_handler/pantry_chef_environment_created_event_handler'
 
 describe Wonga::Daemon::PantryChefEnvironmentCreatedEventHandler do
-  it_behaves_like 'handler'
-  describe "#handle_message" do
-    #add in unit test code for handle message here.
+  let(:api_client) { instance_double('Wonga::Daemon::PantryApiClient').as_null_object }
+  let(:logger) { instance_double('Logger').as_null_object }
+  subject { described_class.new(api_client,logger) }
+  let(:message) { { 'pantry_request_id' => 42, 'instance_id' => 24 } }
+  it_behaves_like "handler"
+
+  it "updates Pantry using PantryApiClient" do
+    expect(api_client).to receive(:update_ec2_instance).with(42, { instance_id: 24, environment_created: true })
+    subject.handle_message(message)
   end
 end
 
