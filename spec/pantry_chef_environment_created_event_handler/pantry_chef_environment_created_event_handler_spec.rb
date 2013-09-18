cb9f39ccdc198dc3f6b2d26cb1e46564e0a09378
Kieran Manning <kieran.manning@wonga.com>
2013-09-18 18:22:32 +0100

    jiggling message sending

--- spec/pantry_chef_environment_created_event_handler/pantry_chef_environment_created_event_handler_spec.rb
+++ spec/pantry_chef_environment_created_event_handler/pantry_chef_environment_created_event_handler_spec.rb
@@ -5,11 +5,18 @@ describe Wonga::Daemon::PantryChefEnvironmentCreatedEventHandler do
   let(:api_client) { instance_double('Wonga::Daemon::PantryApiClient').as_null_object }
   let(:logger) { instance_double('Logger').as_null_object }
   subject { described_class.new(api_client,logger) }
-  let(:message) { { 'pantry_request_id' => 42, 'instance_id' => 24 } }
+  let(:message) { 
+    { 
+      "team_id" => 42, 
+      "chef_environment" => "some value" 
+    } 
+  }
   it_behaves_like "handler"
 
   it "updates Pantry using PantryApiClient" do
-    expect(api_client).to receive(:update_ec2_instance).with(42, { instance_id: 24, environment_created: true })
+    expect(api_client).to receive(:send_post_request).with(
+      "/api/teams/42/chef_environments",
+      { name: "some value"})
     subject.handle_message(message)
   end
 end
